982143ac6500f5234a3f59cf564dd5f65ba9e583
Bharat <bharatappguru@gmail.com>
2015-02-11 18:05:57 +0530

    Ads Implementation In Unity Using AppLovin

---
+++ Assets/Scripts/AdManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AppLovin.SetSdkKey("YOUR_SDK_KEY");   // Paste Your SDK Key
+        AppLovin.InitializeSdk();
+        AppLovin.SetUnityAdListener(gameObject.name);
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 200, 50), "Show Banner Ad"))
+        {
+            AppLovin.ShowAd();
+        }
+        if (GUI.Button(new Rect(0, 75, 200, 50), "Catch Interstitial"))
+        {
+            AppLovin.PreloadInterstitial();
+        }
+        if (GUI.Button(new Rect(0, 150, 200, 50), "Show Interstitial Ad"))
+        {
+            AppLovin.ShowInterstitial();
+        }
+        if (GUI.Button(new Rect(0, 225, 200, 50), "Hide Ad"))
+        {
+            AppLovin.HideAd();
+        }
+    }
+}
---
+++ Assets/Scripts/AppLovinAdEventListener.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppLovinAdEventListener : MonoBehaviour
+{
+    void onAppLovinEventReceived(string ev)
+    {
+        Debug.Log(ev);
+
+        if (ev.Contains("REWARDAPPROVEDINFO"))
+        {
+            // Split the string into its three components.
+            char[] delimiter = { '|' };
+            string[] split = ev.Split(delimiter);
+
+            // Pull out and parse the amount of virtual currency.
+            double amount = double.Parse(split[1]);
+
+            // Pull out the name of the virtual currency
+            string currencyName = split[2];
+
+            // Do something with this info - for example, grant coins to the user
+            //myFunctionToUpdateBalance(currencyName, amount);
+        }
+
+        if (string.Equals(ev, "DISPLAYEDINTER") || string.Equals(ev, "VIDEOBEGAN"))
+        {
+            Time.timeScale = 0.0f;
+            AudioListener.pause = true;
+        }
+
+        if (string.Equals(ev, "HIDDENINTER") || string.Equals(ev, "VIDEOSTOPPED"))
+        {
+            Time.timeScale = 1.0f;
+            AudioListener.pause = false;
+        }
+
+        if (string.Equals(ev, "LOADEDINTER"))
+        {
+            // The last ad load was successful.
+            // Probably do AppLovin.ShowInterstitial();
+        }
+        if (string.Equals(ev, "LOADFAILED"))
+        {
+            // The last ad load failed.
+        }
+    }
+}
